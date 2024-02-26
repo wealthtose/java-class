b5172ebdd802baeed30d435c574c6792e7d4b71d
Wealth <ehiarekhianoziegbe@gmail.com>
2024-02-26 13:09:19 +0100

    generate random numbers

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+//package src.main;
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
@@ -10,7 +11,16 @@ public class Main {
         for (int i = 1; i <= 10; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+            System.out.println("i = " + i + " random number generated " + generateRandoNumber());
         }
+    }
+
+    private static int generateRandoNumber() {
+
+        Random rand = new Random();
+
+        int upperBound = 10;
+
+        return rand.nextInt(upperBound);
     }
 }
